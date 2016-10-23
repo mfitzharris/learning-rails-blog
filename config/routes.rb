d9a2bae536c0441454187a9d31bc0b509c32aa20
mfitzharris <molly.fitzharris@gmail.com>
2016-10-23 13:39:33 -0500

    Implement commenting ability

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+
   root "posts#index" 
 end
+
+
+#            Prefix Verb   URI Pattern                                 Controller#Action
+#     post_comments GET    /posts/:post_id/comments(.:format)          comments#index
+#                   POST   /posts/:post_id/comments(.:format)          comments#create
+#  new_post_comment GET    /posts/:post_id/comments/new(.:format)      comments#new
+# edit_post_comment GET    /posts/:post_id/comments/:id/edit(.:format) comments#edit
+#      post_comment GET    /posts/:post_id/comments/:id(.:format)      comments#show
+#                   PATCH  /posts/:post_id/comments/:id(.:format)      comments#update
+#                   PUT    /posts/:post_id/comments/:id(.:format)      comments#update
+#                   DELETE /posts/:post_id/comments/:id(.:format)      comments#destroy
+#             posts GET    /posts(.:format)                            posts#index
+#                   POST   /posts(.:format)                            posts#create
+#          new_post GET    /posts/new(.:format)                        posts#new
+#         edit_post GET    /posts/:id/edit(.:format)                   posts#edit
+#              post GET    /posts/:id(.:format)                        posts#show
+#                   PATCH  /posts/:id(.:format)                        posts#update
+#                   PUT    /posts/:id(.:format)                        posts#update
+#                   DELETE /posts/:id(.:format)                        posts#destroy
+#              root GET    /                                           posts#index
