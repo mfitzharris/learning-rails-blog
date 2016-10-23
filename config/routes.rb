6fbee680e1185dfb0b886bab7e0a186719458bea
mfitzharris <molly.fitzharris@gmail.com>
2016-10-23 14:11:05 -0500

    Create pages with about view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   end
 
   root "posts#index" 
+
+  get '/about', to: 'pages#about'
 end
+
 
 
 #            Prefix Verb   URI Pattern                                 Controller#Action
