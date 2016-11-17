3908fc4b84b85b0fd95730005a3dc1a3a33ca90f
mfitzharris <molly.fitzharris@gmail.com>
2016-11-17 17:27:50 -0600

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,24 +13,3 @@ Rails.application.routes.draw do
   delete '/sessions' => 'sessions#destroy'
 
 end
-
-
-
-#            Prefix Verb   URI Pattern                                 Controller#Action
-#     post_comments GET    /posts/:post_id/comments(.:format)          comments#index
-#                   POST   /posts/:post_id/comments(.:format)          comments#create
-#  new_post_comment GET    /posts/:post_id/comments/new(.:format)      comments#new
-# edit_post_comment GET    /posts/:post_id/comments/:id/edit(.:format) comments#edit
-#      post_comment GET    /posts/:post_id/comments/:id(.:format)      comments#show
-#                   PATCH  /posts/:post_id/comments/:id(.:format)      comments#update
-#                   PUT    /posts/:post_id/comments/:id(.:format)      comments#update
-#                   DELETE /posts/:post_id/comments/:id(.:format)      comments#destroy
-#             posts GET    /posts(.:format)                            posts#index
-#                   POST   /posts(.:format)                            posts#create
-#          new_post GET    /posts/new(.:format)                        posts#new
-#         edit_post GET    /posts/:id/edit(.:format)                   posts#edit
-#              post GET    /posts/:id(.:format)                        posts#show
-#                   PATCH  /posts/:id(.:format)                        posts#update
-#                   PUT    /posts/:id(.:format)                        posts#update
-#                   DELETE /posts/:id(.:format)                        posts#destroy
-#              root GET    /                                           posts#index
